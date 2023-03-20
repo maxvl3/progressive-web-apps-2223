0f0d129a6f4dc17f55cb396db1dca1acc4392b75
Max <maxvanliempdt@gmail.com>
2023-03-20 11:24:27 +0100

    basis app

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { engine } = require("express-handlebars");
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use("/static", express.static("static"));
+
 app.get('/', (req, res) => {
-  res.send('Dit is een test')
+    res.render("test");
 })
 
 app.listen(port, () => {
