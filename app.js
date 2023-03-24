881a0fbf8edc02f2e1ab174c4ad04eab4280d570
Max <maxvanliempdt@gmail.com>
2023-03-24 11:58:36 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 const { engine } = require("express-handlebars");
 app.engine("handlebars", engine());
@@ -10,14 +10,23 @@ app.set("views", "./views");
 app.use("/static", express.static("static"));
 
 app.get('/', async (req, res) => {
-    await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&p=1&ps=10')
+    await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&involvedMaker=Rembrandt+van+Rijn&p=1&ps=10')
     .then(res => res.json())
     .then(json => {
         const kunstwerken = json.artObjects;
-        res.render("test", { kunstwerken });
+        res.render("home", { kunstwerken });
     })
-})
+});
+
+app.get('/:objectNumber', async (req, res) => {
+  await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&q=' + req.params.objectNumber)
+    .then(res => res.json())
+    .then(json => {
+        const kunstwerk = json.artObjects;
+        res.render("detail", { kunstwerk });
+    })
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
