b8a794d2c52e440530dd51b164dded972c588d88
Max <maxvanliempdt@gmail.com>
2023-03-27 11:34:59 +0200

    manifest

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const { engine } = require("express-handlebars");
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
-app.use("/static", express.static("static"));
+app.use(express.static("static"));
 
 app.get('/', async (req, res) => {
     await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&involvedMaker=Rembrandt+van+Rijn&p=1&ps=10')
@@ -18,13 +22,22 @@ app.get('/', async (req, res) => {
     })
 });
 
-app.get('/:objectNumber', async (req, res) => {
+app.get('/artwork/:objectNumber', async (req, res) => {
   await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&q=' + req.params.objectNumber)
     .then(res => res.json())
     .then(json => {
         const kunstwerk = json.artObjects;
         res.render("detail", { kunstwerk });
     })
+});
+
+app.post("/search", async (req, res) => {
+  await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&q=' + req.body.input)
+  .then(res => res.json())
+  .then(json => {
+      const searchresult = json.artObjects;
+      res.render("search", { searchresult });
+  })
 });
 
 app.listen(port, () => {
