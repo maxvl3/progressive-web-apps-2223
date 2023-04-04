cc34cd11835042aec9794cff08c4cc1066acf660
Max <maxvanliempdt@gmail.com>
2023-04-05 01:45:12 +0200

    ready

--- app.js
+++ app.js
@@ -1,49 +1,77 @@
-const express = require('express');
+// Importeer de express-module en initialiseer de app
+const express = require("express");
 const app = express();
+
+// Stel de poort in waarnaar geluisterd moet worden
 const port = 3000;
 
+// Performance upgrade doormiddel van compressie
+const compression = require("compression");
+app.use(compression());
+
+// Importeer de body-parser-module voor het verwerken van de request body
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Importeer de express-handlebars-module als view engine
 const { engine } = require("express-handlebars");
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+// Stel de map in waarin statische bestanden zich bevinden
 app.use(express.static("static"));
 
-app.get('/manifest.json', (req, res) => {
-  res.sendFile(__dirname + '/manifest.json');
+// Definieer een endpoint om het manifest-bestand te serveren
+app.get("/manifest.json", (req, res) => {
+  res.sendFile(__dirname + "/manifest.json");
 });
 
-app.get('/', async (req, res) => {
-    await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&involvedMaker=Rembrandt+van+Rijn&p=1&ps=10')
-    .then(res => res.json())
-    .then(json => {
-        const kunstwerken = json.artObjects;
-        res.render("home", { kunstwerken });
-    })
+// Definieer de homepagina en haal data op van de Rijksmuseum API
+app.get("/", async (req, res) => {
+  await fetch(
+    "https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&involvedMaker=Rembrandt+van+Rijn&p=1&ps=10"
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      const kunstwerken = json.artObjects;
+      res.render("home", { kunstwerken });
+    });
 });
 
-app.get('/artwork/:objectNumber', async (req, res) => {
-  await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&q=' + req.params.objectNumber)
-    .then(res => res.json())
-    .then(json => {
-        const kunstwerk = json.artObjects;
-        res.render("detail", { kunstwerk });
-    })
+// Definieer een endpoint voor de detailpagina van een kunstwerk en haal data op van de Rijksmuseum API
+app.get("/artwork/:objectNumber", async (req, res) => {
+  await fetch(
+    "https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&q=" +
+      req.params.objectNumber
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      const kunstwerk = json.artObjects;
+      res.render("detail", { kunstwerk });
+    });
 });
 
+// Definieer een endpoint voor de zoekfunctie en haal data op van de Rijksmuseum API
 app.post("/search", async (req, res) => {
-  await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&q=' + req.body.input)
-  .then(res => res.json())
-  .then(json => {
+  await fetch(
+    "https://www.rijksmuseum.nl/api/nl/collection?key=yLfBqOT3&imgonly=true&ps=20&s=relevance&q=" +
+      req.body.input
+  )
+    .then((res) => res.json())
+    .then((json) => {
       const searchresult = json.artObjects;
-      res.render("search", { searchresult });
-  })
+      res.render("search", { searchresult, searchinput: req.body.input });
+    });
 });
 
+// Definieer een offline pagina
+app.get("/offline", (req, res) => {
+  res.render("offline");
+});
+
+// Start de server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,41 +1,80 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      function (registration) {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function (err) {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
-
-const CACHE_NAME = "my-site-cache-v1";
-const urlsToCache = ["/style/style.css"];
+const CACHE_NAME = "my-site-cache-v1"; // De naam van de cache, deze kan worden gewijzigd bij updates van de site
+const urlsToCache = [
+  // Lijst van URL's die moeten worden opgeslagen in de cache
+  "/",
+  "/offline", // De fallback pagina die wordt weergegeven als de gebruiker offline is
+];
 
 self.addEventListener("install", function (event) {
-  // Perform install steps
+  // Installatie van de service worker
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+    caches
+      .open(CACHE_NAME) // Open de cache
+      .then(function (cache) {
+        console.log("Opened cache"); // Output naar de console
+        return cache.addAll(urlsToCache); // Voeg de URL's toe aan de cache
+      })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  // Activering van de service worker
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            return (
+              cacheName.startsWith("my-site-cache-") && // Verwijder alle caches behalve de huidige
+              cacheName !== CACHE_NAME
+            );
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
     })
   );
 });
 
 self.addEventListener("fetch", function (event) {
+  // Fetch event listener
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+    caches
+      .match(event.request) // Zoek in de cache naar de aangevraagde bron
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          // Als de bron in de cache wordt gevonden
+          return response; // Geef de bron terug vanuit de cache
+        }
+
+        return fetch(event.request) // Als de bron niet in de cache is gevonden, haal de bron op van het internet
+          .then(function (response) {
+            // Check if we received a valid response
+            if (
+              !response ||
+              response.status !== 200 ||
+              response.type !== "basic"
+            ) {
+              // Als het antwoord niet geldig is
+              return response; // Geef het ongeldige antwoord terug
+            }
+
+            // Clone the response
+            var responseToCache = response.clone(); // Maak een kloon van het antwoord
+
+            caches.open(CACHE_NAME).then(function (cache) {
+              // Voeg de bron toe aan de cache
+              cache.put(event.request, responseToCache);
+            });
+
+            return response;
+          })
+          .catch(function () {
+            // Als er een fout optreedt bij het ophalen van de bron, gebruik de fallback pagina
+            return caches.match("/offline");
+          });
+      })
   );
 });
-
